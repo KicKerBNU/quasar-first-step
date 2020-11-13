742405382af50282edd10048b4663398db5cee2f
Everton Alcides Buzzi <evertonbuzzi@gmail.com>
2020-11-12 21:17:30 -0300

    EB:Routs

---
+++ src/pages/Curriculum.vue
@@ -0,0 +1,9 @@
+<template>
+	<div>COMPNENTE CURRICULUM</div>
+</template>
+
+<script>
+	export default {};
+</script>
+
+<style></style>
---
+++ src/pages/FindMe.vue
@@ -0,0 +1,9 @@
+<template>
+	<div>Componente FIND ME</div>
+</template>
+
+<script>
+	export default {};
+</script>
+
+<style></style>
