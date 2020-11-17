94da7599e2709fabd207213122199ccf59fbe275
Everton Alcides Buzzi <evertonbuzzi@gmail.com>
2020-11-17 00:25:08 -0300

    EB: Update homepage

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,7 +1,42 @@
 <template>
-	<q-page class="flex flex-center">
-		<img alt="Everton photo" src="~assets/everton_t.png" class="home" />
-	</q-page>
+	<div class="q-pa-md q-gutter-md container">
+		<q-card
+			class="my-card"
+			style="background: radial-gradient(circle, #EBEBF3 0%, #FFFF 100%)"
+		>
+			<q-card-section>
+				<h5>Hi, im a Frontend developer</h5>
+				<p>
+					All you find on this website is done with VueJS, a Javascript
+					framework focused on building SPA.
+				</p>
+				<p>Hope you enjoy, fell free to contact me anytime</p>
+			</q-card-section>
+		</q-card>
+
+		<div class="row justify-between">
+			<q-parallax
+				src="https://firebasestorage.googleapis.com/v0/b/fin-backend.appspot.com/o/christopher-gower-m_HRfLhgABo-unsplash.jpg?alt=media"
+			>
+			</q-parallax>
+		</div>
+		<q-card
+			class="my-card text-white vertical-middle"
+			style="background: radial-gradient(circle, #35a2ff 0%, #014a88 100%)"
+		>
+			<q-card-section class="flex flex-center">
+				<div class="text-h6">
+					Keep studing, keep learning and never give up!
+				</div>
+			</q-card-section>
+			<img alt="VueJS" src="~assets/vuejs.jpg" />
+
+			<!-- <q-card-section class="q-pt-none">
+				<img alt="Everton photo" src="~assets/everton_t.png" />
+			</q-card-section> -->
+		</q-card>
+		<q-page> </q-page>
+	</div>
 </template>
 
 <script>
@@ -10,7 +45,7 @@
 	};
 </script>
 <style scoped>
-	.home {
-		width: 35%;
+	.container {
+		margin: 2%;
 	}
 </style>
